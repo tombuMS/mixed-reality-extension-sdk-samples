3bb1c992484d3151380022634e2a41610746a815
Tom Burdak <tombu@microsoft.com>
2020-07-23 08:18:16 -0700

    Skip after functionality

--- samples/youtube-player/src/mediaPlayer.ts
+++ samples/youtube-player/src/mediaPlayer.ts
@@ -5,6 +5,7 @@
 
 import * as MRE from '@microsoft/mixed-reality-extension-sdk';
 import { MediaType, ImageMedia, VideoMedia, StreamingMedia } from './media';
+import MediaManager from './mediaManager';
 
 export enum PlaybackState {
 	Stopped = 'stopped',
@@ -19,24 +20,37 @@ export class MediaPlayer {
 
     private _currentMedia: StreamingMedia = null;
     private _playbackState = PlaybackState.Stopped;
+
+    private _skipTimeout: NodeJS.Timeout = null;
     
-    private imageHost: any;
+    // Will be used to host the image
+    private imageHost: MRE.Actor;
 
     public get currentMediaType() { return this._currentMedia?.type; }
     public get playbackState() { return this._playbackState; }
 
-    constructor(private rootActor: MRE.Actor, private assets: MRE.AssetContainer) {
+    constructor(private rootActor: MRE.Actor, private assets: MRE.AssetContainer, private _mediaManager: MediaManager) {
 
     }
 
-    // public start(mediaType: MediaType.Image, media: ImageMediaLike): void;
-    // public start(mediaType: MediaType.Video, media: VideoMediaLike): void;
-    public start(media: ImageMedia | VideoMedia): void {
+    public start(): void {
+        this._currentMedia = this._currentMedia ?? this._mediaManager.currentMedia();
+        if (!this._currentMedia) {
+            return;
+        }
+
+        // Clear out any skip timer that might exist.
+        this.clearSkipTimeout();
+
         // Clear out the video and image hosts.
         delete this._videoPlayerInstance;
 
-        if (media.type === MediaType.Video) {
-            this.startVideo(media as VideoMedia);
+        if (this._currentMedia.type === MediaType.Video) {
+            this.startVideo(this._currentMedia as VideoMedia);
+        }
+
+        if (this._currentMedia.skipAfter > 0) {
+            this._skipTimeout = setTimeout(_ => { this.next() }, this._currentMedia.skipAfter * 1000);
         }
 
         this._playbackState = PlaybackState.Playing;
@@ -46,6 +60,8 @@ export class MediaPlayer {
         if (this._videoPlayerInstance) {
             this._videoPlayerInstance.stop();
         }
+
+        this.clearSkipTimeout();
 
         this._playbackState = PlaybackState.Stopped;
     }
@@ -66,8 +82,28 @@ export class MediaPlayer {
         this._playbackState = PlaybackState.Playing;
     }
 
+    public next() {
+        this.stop()
+        this._currentMedia = this._mediaManager.nextMedia();
+        this.start();
+    }
+
+    public previous() {
+        this.stop();
+        this._currentMedia = this._mediaManager.perviousMedia();
+        this.start();
+    }
+
     //=======================================================================================================================
     // Helper Functions
+    private clearSkipTimeout() {
+        if (this._skipTimeout) {
+            clearTimeout(this._skipTimeout);
+            delete this._skipTimeout;
+            this._skipTimeout = null;
+        }
+    }
+
     private startVideo(video: VideoMedia) {
         this._videoStreamCache[video.url] = this._videoStreamCache[video.url] 
             ?? this.assets.createVideoStream(`${video.url}`, {
@@ -80,5 +116,9 @@ export class MediaPlayer {
             time: video.startTime,
             looping: video.loop
         });
+    }
+
+    private startImage(image: ImageMedia) {
+        
     }
 }
