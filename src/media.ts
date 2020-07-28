e4656e335328600f50feddd12b46746ae3319499
Tom Burdak <tombu@microsoft.com>
2020-07-28 10:45:11 -0700

    Convert spaces to tabs

--- src/media.ts
+++ src/media.ts
@@ -7,85 +7,85 @@ import * as MRE from '@microsoft/mixed-reality-extension-sdk';
 import { ImageConfig, VideoConfig } from "./mediaConfig";
 
 export enum MediaType {
-    Video = 'video',
-    Image = 'image'
+	Video = 'video',
+	Image = 'image'
 }
 
 export interface StreamingMediaLike {
-    type: MediaType;
-    url: string;
-    asset: MRE.Asset;
-    skipAfter: number;
+	type: MediaType;
+	url: string;
+	asset: MRE.Asset;
+	skipAfter: number;
 
-    preloadAssets(assetContainer: MRE.AssetContainer): Promise<void>;
+	preloadAssets(assetContainer: MRE.AssetContainer): Promise<void>;
 }
 
 export abstract class StreamingMedia<AssetT extends MRE.Asset> implements StreamingMediaLike {
-    protected _asset: AssetT;
+	protected _asset: AssetT;
 
-    public abstract get type(): MediaType;
+	public abstract get type(): MediaType;
 
-    public get asset() { return this._asset; };
-    public get url() { return this._url; }
-    public get skipAfter() { return this._skipAfter; }
+	public get asset() { return this._asset; };
+	public get url() { return this._url; }
+	public get skipAfter() { return this._skipAfter; }
 
-    public constructor(private _url: string, private _skipAfter: number = null) {
+	public constructor(private _url: string, private _skipAfter: number = null) {
 
-    }
+	}
 
-    public abstract preloadAssets(assetContainer: MRE.AssetContainer): Promise<void>;
+	public abstract preloadAssets(assetContainer: MRE.AssetContainer): Promise<void>;
 }
 
 export class ImageMedia extends StreamingMedia<MRE.Texture> {
-    public get type() { return MediaType.Image; }
+	public get type() { return MediaType.Image; }
 
-    public constructor(config: Partial<ImageConfig>) {
-        super(config?.imageUrl, config?.skipAfter);
-    }
+	public constructor(config: Partial<ImageConfig>) {
+		super(config?.imageUrl, config?.skipAfter);
+	}
 
-    public preloadAssets(assetContainer: MRE.AssetContainer): Promise<void> {
-        this._asset = assetContainer.createTexture('image', {
-            uri: this.url,
-            wrapU: MRE.TextureWrapMode.Clamp,
-            wrapV: MRE.TextureWrapMode.Clamp
-        });
+	public preloadAssets(assetContainer: MRE.AssetContainer): Promise<void> {
+		this._asset = assetContainer.createTexture('image', {
+			uri: this.url,
+			wrapU: MRE.TextureWrapMode.Clamp,
+			wrapV: MRE.TextureWrapMode.Clamp
+		});
 
-        // Return a wrapper promise that will create the image host actor and cache it?
-        return this._asset.created;
-    }
+		// Return a wrapper promise that will create the image host actor and cache it?
+		return this._asset.created;
+	}
 }
 
 export class VideoMedia extends StreamingMedia<MRE.VideoStream> {
-    private _volume = .5;
-    private _startTime = 0;
-    private _loop = false;
-    private _rolloffStartDistance = 1.0;
+	private _volume = .5;
+	private _startTime = 0;
+	private _loop = false;
+	private _rolloffStartDistance = 1.0;
 
-    public get type() { return MediaType.Video; }
+	public get type() { return MediaType.Video; }
 
-    public get volume() { return this._volume; }
-    public get startTime() { return this._startTime; }
-    public get loop() { return this._loop; }
-    public get rolloffStartDistance() { return this._rolloffStartDistance; }
+	public get volume() { return this._volume; }
+	public get startTime() { return this._startTime; }
+	public get loop() { return this._loop; }
+	public get rolloffStartDistance() { return this._rolloffStartDistance; }
 
-    public constructor(config: Partial<VideoConfig>) {
-        super(config?.videoUrl, config?.skipAfter);
-        this.init(config);
-    }
+	public constructor(config: Partial<VideoConfig>) {
+		super(config?.videoUrl, config?.skipAfter);
+		this.init(config);
+	}
 
-    public preloadAssets(assetContainer: MRE.AssetContainer): Promise<void> {
-        this._asset = assetContainer.createVideoStream("video-stream", {
-            uri: this.url
-        });
+	public preloadAssets(assetContainer: MRE.AssetContainer): Promise<void> {
+		this._asset = assetContainer.createVideoStream("video-stream", {
+			uri: this.url
+		});
 
-        return this._asset.created;
-    }
+		return this._asset.created;
+	}
 
-    private init(config: Partial<VideoConfig>): this {
-        if (!config) { return this; }
-        if (config.volume) { this._volume = config.volume; }
-        if (config.startTime) { this._startTime = config.startTime; }
-        if (config.loop) { this._loop = config.loop; }
-        if (config.rolloffStartDistance) { this._rolloffStartDistance = config.rolloffStartDistance; }
-    }
+	private init(config: Partial<VideoConfig>): this {
+		if (!config) { return this; }
+		if (config.volume) { this._volume = config.volume; }
+		if (config.startTime) { this._startTime = config.startTime; }
+		if (config.loop) { this._loop = config.loop; }
+		if (config.rolloffStartDistance) { this._rolloffStartDistance = config.rolloffStartDistance; }
+	}
 }
